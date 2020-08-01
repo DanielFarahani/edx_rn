ad0eb5d67c27b4dfa6c98423f2951c35ee8828c6
danielfarahani <danfarahani@gmail.com>
2020-08-01 17:36:42 +1000

    simulator running, timer attemp

--- project1/App.js
+++ project1/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {View, Text} from './components'
+
+const DEFAULT_MINS = 10
+const DEFAULT_SECS = 0
+
+const Timer = props => (
+  <div>
+    <span>{props.time.minutes}</span>
+    <span> : </span>
+    <span>{props.time.seconds}</span>
+  </div>
+)
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      minutes: DEFAULT_MINS,
+      seconds: DEFAULT_SECS
+    }
+  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+    <h1>WORK TIMER</h1>
+    <Timer style={styles.container} time={() => this.state}></Timer>
     );
   }
 }
